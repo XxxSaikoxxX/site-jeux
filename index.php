e0a9350ff909e01e5a22a3cd1d0468df3a41dba9
XxxSaikoxxX <sparadis35@gmail.com>
2023-12-20 13:05:01 -0500

    index final

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php
-require './config/connexion.php';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Page Principale - Jeux Vidéo</title>
+    <link rel="stylesheet" type="text/css" href="css/jeuxvideo.css">
+</head>
+
+<body>
+    <?php
+    include './includes/fonction.php';
+    include './public/header.php';
+    ?>
+    <br><br>
+    <div class="bodyHome">
+        <h1><u>Les Jeux Vidéo</u></h1><br>
+        <p>Les jeux vidéo sont une forme passionnante de divertissement et d'expression artistique, offrant une immersion dans des mondes virtuels divers et captivants. Ils combinent technologie, narration, art visuel et interactivité pour créer des expériences uniques.</p><br>
+
+        <h2><u>Développement des Jeux Vidéo :</u></h2><br>
+        <p>Le développement de jeux vidéo est un processus complexe qui implique la programmation, le design graphique, la narration et la composition musicale. Cela peut varier d'un petit projet indépendant à de grandes productions de studios. Les développeurs de jeux utilisent des moteurs de jeu avancés et des outils de création pour apporter leurs visions créatives à la vie.</p>
+
+        <p>Les jeux vidéo modernes offrent une grande variété de genres, allant des jeux d'action et d'aventure aux simulations, en passant par les jeux de stratégie, les jeux de rôle et les puzzles. Chaque genre offre une expérience unique, attirant un large éventail de joueurs aux intérêts divers.</p>
+
+        <h3><u>Histoire des Jeux Vidéo :</u></h3><br>
+        <p>L'histoire des jeux vidéo remonte aux années 1950, avec des innovations qui ont posé les fondations pour les jeux modernes. Depuis lors, l'industrie a connu une évolution rapide, avec des avancées technologiques qui ont transformé la façon dont nous jouons. Des premières consoles de jeux aux systèmes de réalité virtuelle d'aujourd'hui, les jeux vidéo ont parcouru un long chemin.</p>
+
+        <p>Les jeux vidéo ont également eu un impact culturel significatif, influençant la musique, les films et d'autres formes d'art. Ils sont devenus une partie intégrante de la culture populaire, avec une communauté mondiale de joueurs et des événements compétitifs, tels que les tournois d'esports, qui attirent des millions de spectateurs du monde entier.</p>
+
+    </div>
+</body>
+
+</html>
